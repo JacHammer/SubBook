e345eb62a17570562c87386c52680a10cdf46968
Minci Zhou <zmc1996@hotmail.com>
2018-02-04 22:03:36 -0700

    Added AddSub activity

--- app/src/main/java/subbook/subbook/SubscriptionList.java
+++ app/src/main/java/subbook/subbook/SubscriptionList.java
@@ -1,18 +1,30 @@
 package subbook.subbook;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class SubscriptionList extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription_list);
+
+        Button addButton = (Button) findViewById(R.id.button);
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(SubscriptionList.this, AddSub.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 
 }
